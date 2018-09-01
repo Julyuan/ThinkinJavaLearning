25d5182708dfe3a57785bfba749b7e98aeb1e305
Jin Lianyuan <840806300@qq.com>
2018-09-01 12:42:26 +0800

    add doc

---
+++ src/Chapter9/interfaces/Factories.java
@@ -0,0 +1,50 @@
+package Chapter9.interfaces;
+
+import static net.mindview.util.Print.print;
+
+interface Service{
+    void method1();
+    void method2();
+}
+
+interface ServiceFactory{
+    Service getService();
+}
+
+class Implementation1 implements Service{
+    Implementation1(){}
+    public void method1(){print("1 method1");}
+    public void method2(){print("1 method2");}
+}
+
+class Implementation1Factory implements ServiceFactory{
+    public Service getService(){
+        return new Implementation1();
+    }
+}
+
+class Implementation2 implements Service{
+    Implementation2(){}
+    public void method1(){print("2 method1");}
+    public void method2(){print("2 method2");}
+}
+
+class Implementation2Factory implements ServiceFactory {
+    public Service getService() {
+        return new Implementation2();
+    }
+}
+
+public class Factories {
+        public static void serviceConsumer(ServiceFactory fact){
+            Service s = fact.getService();
+            s.method1();
+            s.method2();
+        }
+
+        public static void main(String[] args){
+            serviceConsumer(new Implementation1Factory());
+            serviceConsumer(new Implementation2Factory());
+        }
+}
+
