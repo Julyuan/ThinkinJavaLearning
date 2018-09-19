2f235bcc82e9973d4941d47149c82c6ef48000aa
Jin Lianyuan <840806300@qq.com>
2018-09-19 13:41:21 +0800

    Add files via upload

---
+++ src/Chapter10/innerclasses/TestParcel.java
@@ -0,0 +1,39 @@
+package Chapter10.innerclasses;
+
+import sun.security.krb5.internal.crypto.Des;
+
+class Parcel4{
+    private class PContents implements Contents{
+        private int i=11;
+        public int value(){
+            return i;
+        }
+    }
+    protected class PDestination implements Destination{
+        private String label;
+        private PDestination(String whereTo){
+            label = whereTo;
+        }
+        public String readLabel(){
+            return label;
+        }
+    }
+
+    public Destination destination(String s){
+        return new PDestination(s);
+    }
+
+    public Contents contents(){
+        return new PContents();
+    }
+
+}
+public class TestParcel {
+    public static void main(String[] args){
+        Parcel4 p = new Parcel4();
+        Contents c = p.contents();
+        Destination d = p.destination("Tasmania");
+
+    }
+
+}
