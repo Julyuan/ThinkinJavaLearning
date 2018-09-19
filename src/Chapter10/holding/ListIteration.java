cabe20a1ca3af3ad316dc679a98b649a4e84040e
Jin Lianyuan <jinlianyuan@JindeMacBook-Pro.local>
2018-09-20 00:57:26 +0800

    Add chapter 12

---
+++ src/Chapter10/holding/ListIteration.java
@@ -0,0 +1,5 @@
+package Chapter10.holding;
+
+
+public class ListIteration {
+}
---
+++ src/Chapter12/exceptions/Human.java
@@ -0,0 +1,29 @@
+package Chapter12.exceptions;
+
+class Annoyance extends Exception{}
+class Sneeze extends Annoyance{}
+
+
+public class Human {
+    public static void main(String[] args){
+        try{
+            throw new Sneeze();
+        }catch (Sneeze s){
+            System.out.println("Caught Sneeze");
+        }catch (Annoyance a){
+            System.out.println("Caught Annoyance");
+        }
+
+
+        try{
+            throw new Sneeze();
+        }catch (Annoyance a){
+            System.out.println("Caught Annoyance");
+        }
+    }
+
+
+
+}
+
+
